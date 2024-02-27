36f174d95e987e2bc2e896ecff4e2c41c87dbbd4
igniushka <ignasxp@gmail.com>
2024-02-27 23:11:04 +0000

    cleanup

--- back-end/src/main/java/song/catalogue/demo/AES/EncryptionService.java
+++ back-end/src/main/java/song/catalogue/demo/AES/EncryptionService.java
@@ -1,22 +1,23 @@
 package song.catalogue.demo.AES;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.keygen.KeyGenerators;
 import org.springframework.stereotype.Service;
 
-import javax.crypto.*;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
-import java.nio.charset.StandardCharsets;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
-import java.util.Arrays;
 import java.util.Base64;
 
 @Service
@@ -63,48 +64,6 @@ public class EncryptionService {
         return new String(plainText);
     }
 
-    public String encrypt(final String data) {
-
-
-        byte[] decodedKey = Base64.getDecoder().decode(secret);
-
-        try {
-            Cipher cipher = Cipher.getInstance("AES");
-            // rebuild key using SecretKeySpec
-//            SecretKey originalKey = new SecretKeySpec(Arrays.copyOf(decodedKey, 16), "AES");
-            SecretKey originalKey = createSecretKey("etqS1N6kDTpumgQAVv4ryA==");
-
-
-            cipher.init(Cipher.ENCRYPT_MODE, originalKey);
-            byte[] cipherText = cipher.doFinal(data.getBytes("UTF-8"));
-            return Base64.getEncoder().encodeToString(cipherText);
-        } catch (Exception e) {
-            throw new RuntimeException(
-                    "Error occured while encrypting data", e);
-        }
-
-    }
-
-    public String decrypt(final String encryptedString) {
-
-
-        byte[] decodedKey = Base64.getDecoder().decode(secret);
-
-        try {
-            Cipher cipher = Cipher.getInstance("AES");
-            // rebuild key using SecretKeySpec
-//            SecretKey originalKey = new SecretKeySpec(Arrays.copyOf(decodedKey, 16), "AES");
-            SecretKey originalKey = createSecretKey("etqS1N6kDTpumgQAVv4ryA==");
-            cipher.init(Cipher.DECRYPT_MODE, originalKey);
-            byte[] cipherText = cipher.doFinal(Base64.getDecoder().decode(encryptedString));
-            return new String(cipherText);
-        } catch (Exception e) {
-            throw new RuntimeException(
-                    "Error occured while decrypting data", e);
-        }
-    }
-
-
     private SecretKey createSecretKey(String salt) throws InvalidKeySpecException, NoSuchAlgorithmException {
         //      create secretKey from secret and salt
         SecretKeyFactory factory = SecretKeyFactory.getInstance(SECRET_KEY_FACTORY);
@@ -115,10 +74,8 @@ public class EncryptionService {
     private IvParameterSpec createIV() {
         //        create initialisation vector
         byte[] initialisationVector = new byte[16];
-//        new SecureRandom().nextBytes(initialisationVector);
         return  new IvParameterSpec(initialisationVector);
     }
-
 
 
 }
