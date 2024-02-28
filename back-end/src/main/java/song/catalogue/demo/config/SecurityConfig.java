dc151afd9cfe40a2ad2ff8709782b05cfef347d7
igniushka <ignasxp@gmail.com>
2024-02-28 18:50:23 +0000

    cors fixes

--- back-end/src/main/java/song/catalogue/demo/config/SecurityConfig.java
+++ back-end/src/main/java/song/catalogue/demo/config/SecurityConfig.java
@@ -8,6 +8,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -27,13 +32,23 @@ public class SecurityConfig  {
 
 
     @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+
+    @Bean
     SecurityFilterChain web(HttpSecurity http) throws Exception {
         http.csrf().disable();
-//        http.cors();
         http
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers("/admin/**").permitAll()
-                );
+                ).cors(cors -> cors.disable());
 
 //        http.authorizeHttpRequests()
 //        http
--- back-end/src/main/java/song/catalogue/demo/user/AdminUserResource.java
+++ back-end/src/main/java/song/catalogue/demo/user/AdminUserResource.java
@@ -10,9 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import song.catalogue.demo.exception.DuplicateEntityException;
 import song.catalogue.demo.exception.EntityNotFoundException;
 
+@CrossOrigin
 @RestController
 @RequestMapping("admin/user")
 public class AdminUserResource {
