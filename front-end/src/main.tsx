2bb52f938c5e0ac91c2bc4988cd59a812a939b69
igniushka <ignasxp@gmail.com>
2024-02-28 11:35:09 +0000

    routing working

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -2,10 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import {createBrowserRouter} from 'react-router-dom'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+  },
+  {
+    path: '/register',
+    element: <App/>,
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
