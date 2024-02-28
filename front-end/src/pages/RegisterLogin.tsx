57970de6a89f0ef79c71231d32b818b5769aebec
igniushka <ignasxp@gmail.com>
2024-02-28 12:40:42 +0000

    added mui

---
+++ front-end/src/pages/RegisterLogin.tsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom'
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+export default function RegisterLogin(){
+    return <div> 404 page not found
+        <br/>
+        <Button variant="contained">Contained</Button>
+        </div>;
+}
