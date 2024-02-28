efa17063c0ab89a8dd764fc7189fd9ef4096bf6e
igniushka <ignasxp@gmail.com>
2024-02-28 01:52:57 +0000

    initial react files

--- back-end/src/main/java/song/catalogue/demo/config/SecurityConfig.java
+++ back-end/src/main/java/song/catalogue/demo/config/SecurityConfig.java
@@ -1,23 +1,48 @@
-//package song.catalogue.demo.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@EnableWebSecurity
-//@Configuration
-//public class SecurityConfig  {
-//
+package song.catalogue.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig  {
+
 //    @Bean
 //    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .anyRequest()
-//                .authenticated()
-//                .and()
-//                .httpBasic();
+//        http.authorizeRequests(authorizeRequests -> authorizeRequests.anyRequest()
+//                        .authenticated())
+//                .httpBasic(withDefaults())
+//                .formLogin(withDefaults())
+//                .csrf().disable();
 //        return http.build();
 //    }
-//
-//}
+
+
+    @Bean
+    SecurityFilterChain web(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+//        http.cors();
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/admin/**").permitAll()
+                );
+
+//        http.authorizeHttpRequests()
+//        http
+//                .authorizeHttpRequests((authorize) -> authorize
+//                        .requestMatchers("/endpoint").hasAuthority("USER")
+//                        .anyRequest().authenticated()
+//                );
+        // ...
+
+        return http.build();
+    }
+}
