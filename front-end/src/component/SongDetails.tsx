5d0b4721072c9e6546b016b6d4d91c861fd9bb84
igniushka <ignasxp@gmail.com>
2024-03-02 15:41:21 +0000

    updating new entity state

--- front-end/src/component/SongDetails.tsx
+++ front-end/src/component/SongDetails.tsx
@@ -16,6 +16,7 @@ interface Props {
   open: boolean,
   setOpen: (song: boolean) => void,
 }
+
 export const SongDetails: React.FC<Props> =({song, updateSong, editable, open, setOpen}) => {
     // console.log(song)
     // const handleOpen = () => setOpen(true);
@@ -44,7 +45,7 @@ return(
       keepMounted
       open={open}
       onClose={() => setOpen(false)}>
-        <Stack direction="column" sx={style}>
+        <Stack spacing={1} direction="column" sx={style}>
         <TextField
           variant="outlined"
           margin="normal"
@@ -54,7 +55,7 @@ return(
           name="name"
           autoFocus
           value={song.name}
-          onChange={handleUsernameChange}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => updateSong({ ...song, name: event.target.value })}
         />
         <TextField
           variant="outlined"
@@ -65,9 +66,55 @@ return(
           name="artist"
           autoFocus
           value={song.artist}
-          onChange={handleUsernameChange}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => updateSong({ ...song, artist: event.target.value })}
+        /> 
+        <TextField
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        label="Album"
+        name="album"
+        autoFocus
+        value={song.album}
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) => updateSong({ ...song, album: event.target.value })}
         />
-
+        <TextField
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        label="Genre"
+        name="genre"
+        autoFocus
+        value={song.genre}
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) => updateSong({ ...song, genre: event.target.value })}
+      />
+      <Stack spacing={1} direction="row">
+      <TextField sx={{width: "50%"}}
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        label="Release year"
+        name="year"
+        type="number"
+        autoFocus
+        value={song.year}
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) => updateSong({ ...song, year: parseInt(event.target.value) })}
+      />
+        <TextField sx={{width: "50%"}}
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        label="Length (s)"
+        name="length"
+        autoFocus
+        type="number"
+        value={song.length}
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) => updateSong({ ...song, length: parseInt(event.target.value) })}
+      /></Stack>
         </Stack>
     </Modal>
     )
